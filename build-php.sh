c8d6ad44e95f87d9f876b2dde0bba3f9e601f31b
PS <pramilanarawade70@gmail.com>
2024-07-02 14:29:31 +0530

    Update build-php.sh

--- build-php.sh
+++ build-php.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
 
-# Fail the script if any command fails
+# Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Print commands and their arguments as they are executed
+# Print commands and their arguments as they are executed.
 set -x
 
 echo "Starting PHP application build..."
 
-# Install dependencies using Composer
+# Ensure that PHP and Composer are installed
+if ! [ -x "$(command -v php)" ]; then
+  echo "PHP is not installed. Installing..."
+  sudo apt-get update
+  sudo apt-get install -y php
+fi
+
+if ! [ -x "$(command -v composer)" ]; then
+  echo "Composer is not installed. Installing..."
+  EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+  ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+  if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]; then
+      >&2 echo 'ERROR: Invalid installer checksum'
+      rm composer-setup.php
+      exit 1
+  fi
+
+  php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+  rm composer-setup.php
+fi
+
+# Install dependencies and optimize the autoloader
 composer install --no-dev --optimize-autoloader
-
-# Run PHPUnit tests
-php vendor/bin/phpunit
-
-# If you have a build process, such as compiling assets
-# Uncomment the following line if you use a build tool like Gulp, Grunt, or Webpack
-# npm install
-# npm run build
-
-# Package the application (optional)
-# For example, create a zip file of the application
-zip -r myapp.zip . -x "vendor/*" "node_modules/*" "*.git/*" "*.idea/*"
 
 echo "PHP application build completed successfully."
